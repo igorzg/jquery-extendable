2c7e3dfc280e8f2604d315cfc4297111a942ae4a
Igor <igor.zg1987@gmail.com>
2013-05-29 10:55:30 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,41 @@
+
+(function($){
+
+    function Test(options, selector){
+        this._settings = $.extend({
+            a : 'a',
+            b : 'b'
+        }, options);
+        this._selector = $(selector);
+        this._init();
+    }
+
+    /**
+     * Extend prototype
+     */
+    Test.prototype = {
+        /**
+         * Initalize
+         */
+        _init : function(){
+            
+        },
+          /**
+         * Initalize
+         */
+        _private : function(){
+            throw new Error('Not visible');
+        },
+        
+        /**
+         * Public method
+         */
+         pub : function(){
+            return '1';
+         }
+    }
+
+    $.fn.BoxLogic = $.extendable(Test);
+    
+    
+}(jQuery));
